66530239f088cd0715fe74a9cbadf4e62c550c51
png261 <nhphuong.code@gmail.com>
2025-07-17 08:32:13 +0700

    feat: init

---
+++ src/components/Sidebar/chat/markdown-components/Table.tsx
@@ -0,0 +1,22 @@
+import type { ReactMarkdownProps } from 'react-markdown/lib/ast-to-react'
+
+type TableType =
+  | keyof JSX.IntrinsicElements
+  | React.ComponentType<
+      Omit<
+        React.DetailedHTMLProps<
+          React.TableHTMLAttributes<HTMLTableElement>,
+          HTMLTableElement
+        >,
+        'ref'
+      > &
+        ReactMarkdownProps
+    >
+
+export const Table: TableType = (props) => {
+  return (
+    <div className="sidebuddy_table-container">
+      <table {...props} />
+    </div>
+  )
+}
